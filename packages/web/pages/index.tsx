b19f6286e0ff80851f2763d544f2e3f8e0291841
Daniel Steogerwald <daniel@steigerwald.cz>
2019-02-06 22:21:44 +0100

    Test CI

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -7,6 +7,7 @@ import { pageTitles } from './_app';
 const Index: React.FunctionComponent = () => {
   const { intl } = useAppContext();
   const title = intl.formatMessage(pageTitles.index);
+  const a: 1 = 2;
 
   return (
     <Layout title={title}>
