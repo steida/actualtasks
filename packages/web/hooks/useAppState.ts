63566ba2b2d584ac105f563acaddb76f1f2a312b
Daniel Steogerwald <daniel@steigerwald.cz>
2019-02-21 21:18:40 +0100

    Update useAppState.ts

--- packages/web/hooks/useAppState.ts
+++ packages/web/hooks/useAppState.ts
@@ -1,10 +1,12 @@
-// So we don't have to import AppState everywhere.
 import {
   SetAppState,
   useAppState as useAppStateLib,
 } from '@app/shared/lib/appstate';
 import { AppState } from '@app/shared/types';
 
+// Make typed useAppState, so we don't have to:
+//  - import AppState type everywhere
+//  - define Selected state, because it's inferred.
 const useAppState = <SelectedState>(
   selector: (state: AppState) => SelectedState,
 ): [SelectedState, SetAppState<AppState>] => {
