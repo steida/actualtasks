66d7e06f6f78654487cc3e808399deb7e9b02675
Daniel Steogerwald <daniel@steigerwald.cz>
2019-02-22 04:07:09 +0100

    Update useAppState.ts

--- packages/web/hooks/useAppState.ts
+++ packages/web/hooks/useAppState.ts
@@ -1,6 +1,13 @@
 import { AppState } from '@app/shared/appStateConfig';
 import { useAppState as useAppStateLib } from '@app/shared/lib/appstate';
-import { SetAppState } from '@app/shared/lib/appState/AppStateContext';
+import { Draft } from 'immer';
+// import { SetAppState } from '@app/shared/lib/appState/AppStateContext';
+
+// For some reason, Circle CI fails when SetAppState is imported. Immer? Or what?
+// https://github.com/steida/actualtasks/issues/15
+export type SetAppState<S = any> = (
+  callback: (draft: Draft<S>) => void,
+) => void;
 
 // Make typed useAppState version, so we don't have to:
 //  - import AppState type everywhere
