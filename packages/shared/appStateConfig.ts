d09f3ab3c57cb56013d995187ebb317f7eefd64b
Daniel Steogerwald <daniel@steigerwald.cz>
2019-02-23 03:16:22 +0100

    Update appStateConfig.ts

--- packages/shared/appStateConfig.ts
+++ packages/shared/appStateConfig.ts
@@ -1,8 +1,9 @@
+import { DeepReadonly } from 'utility-types';
+
 const name = 'actualtasks';
 
-// Note we don't use arrays for models.
-// https://firebase.googleblog.com/2014/04/best-practices-arrays-in-firebase.html
-// Also, we normalize data. Joins are super cheap on in memory objects.
+// Remember: Never ever change any interface without version migration step.
+
 // To add new AppState version:
 //  - Add new AppStateX, do not save.
 //  - Set AppState to AppStateX, do not save.
@@ -11,18 +12,40 @@ const name = 'actualtasks';
 //  - If TypeScript is happy, save! Hot reloading will update local storage but
 //    not UI, reload is the must.
 
-interface User {
-  readonly email: string;
-  readonly darkMode: boolean;
+interface User1 {
+  email: string;
+  darkMode: boolean;
 }
 
-interface AppState1 {
-  readonly viewer: User;
+interface TaskList1 {
+  id: string;
+  name: string;
+  slate: {
+    document: {
+      nodes: Array<{
+        data: {
+          completed: boolean;
+          depth: number;
+        };
+        nodes: Array<{
+          leaves: Array<{ text: string }>;
+          object: 'text';
+        }>;
+        object: 'block';
+        type: 'task';
+      }>;
+    };
+  };
 }
+
+type AppState1 = DeepReadonly<{
+  viewer: User1;
+  taskLists: TaskList1[];
+}>;
 
 // interface AppState2 {
-//   readonly viewer: User;
-//   readonly fok: string;
+//   viewer: User;
+//   fok: string;
 // }
 
 export type AppState = AppState1;
@@ -30,6 +53,29 @@ export type AppState = AppState1;
 const migrations = [
   (): AppState1 => {
     return {
+      taskLists: [
+        {
+          id: 'actual', // Must be the same both for client and server.
+          name: 'Actual',
+          slate: {
+            document: {
+              nodes: [
+                {
+                  data: { completed: false, depth: 0 },
+                  nodes: [
+                    {
+                      leaves: [{ text: 'What should be done?' }],
+                      object: 'text',
+                    },
+                  ],
+                  object: 'block',
+                  type: 'task',
+                },
+              ],
+            },
+          },
+        },
+      ],
       viewer: {
         darkMode: false,
         email: '',
