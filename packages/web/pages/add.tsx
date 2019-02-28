94a0fe5ab55a7a9e90f1351ceca93c74cf70c64b
Daniel Steogerwald <daniel@steigerwald.cz>
2019-02-28 02:16:08 +0100

    Update add.tsx
    Add add form

--- packages/web/pages/add.tsx
+++ packages/web/pages/add.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
-import { Text, TextInput } from 'react-native';
-// import isEmail from 'validator/lib/isEmail';
-// import Button from '../components/Button';
+import { Text, TextInput, View } from 'react-native';
+// import createTaskList from '@app/state/createTaskList';
+import Button from '../components/Button';
 import Layout from '../components/Layout';
 import useAppContext from '../hooks/useAppContext';
 // import useAppState from '../hooks/useAppState';
@@ -11,20 +11,32 @@ import { pageTitles } from './_app';
 const NameInput: React.FunctionComponent = () => {
   const { theme } = useAppContext();
   const [name, setName] = useState('');
-  // const labelIsValid = email === '' || isEmail(email);
-  const labelIsValid = true;
+  // use setAppState? nebo optional?
+  // const [, setAppState] = useAppState(state => state);
+  const handleSubmitEditing = () => {
+    // if (name.length < 1) return;
+    // const taskList = createTaskList();
+  };
 
   return (
     <>
-      <Text style={[theme.label, !labelIsValid && theme.labelInvalid]}>
+      <Text style={theme.label}>
         <FormattedMessage defaultMessage="Name" id="nameLabel" />
       </Text>
       <TextInput
-        keyboardType="email-address"
         onChangeText={text => setName(text)}
         style={theme.textInputOutline}
+        enablesReturnKeyAutomatically
+        blurOnSubmit={false}
+        onSubmitEditing={handleSubmitEditing}
         value={name}
+        maxLength={32}
       />
+      <View style={[theme.buttons, theme.marginTop]}>
+        <Button onPress={handleSubmitEditing} size="small" type="primary">
+          <FormattedMessage defaultMessage="Add" id="add" />
+        </Button>
+      </View>
     </>
   );
 };
