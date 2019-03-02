0b555b3b5ec1c3ff396ea46835409630bc55a14c
Daniel Steogerwald <daniel@steigerwald.cz>
2019-03-02 04:56:39 +0100

    TaskList name to page title, if any

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,34 +1,23 @@
 import React, { FunctionComponent } from 'react';
-import { WithRouterProps } from 'next/router';
+import { WithRouterProps, withRouter } from 'next/router';
 import Layout from '../components/Layout';
 import useAppContext from '../hooks/useAppContext';
 import { pageTitles } from './_app';
-// import useAppState from '../hooks/useAppState';
+import useAppState from '../hooks/useAppState';
 
-const Index: FunctionComponent<WithRouterProps> = () => {
+const Index: FunctionComponent<WithRouterProps> = ({ router }) => {
   const { intl } = useAppContext();
-  // const viewer = useAppState(state => state.viewer);
-  // // eslint-disable-next-line no-console
-  // console.log(viewer);
+  // TODO: For typed parsing, use gcanti/io-ts.
+  const taskListId = router && router.query && router.query.id;
+  // useAppState selector must be a pure function because it's memoized.
+  const taskLists = useAppState(state => state.taskLists);
+  // TODO: taskList should be maybe null. How?
+  const taskList = taskLists[taskListId as string];
+  const taskListName = taskList && taskList.name;
 
-  const title = intl.formatMessage(pageTitles.index);
+  const title = taskListName || intl.formatMessage(pageTitles.index);
+
   return <Layout title={title}>{/* <Tasks /> */}</Layout>;
 };
 
-export default Index;
-
-// const Index: FunctionComponent<WithRouterProps> = () => {
-//   const { intl } = useAppContext();
-//   // const taskListId = router && router.query && router.query.id;
-//   // const taskList = useAppState(state =>
-//   //   state.taskLists.find(taskList => taskList.id === taskListId),
-//   // );
-//   // eslint-disable-next-line no-console
-//   // console.log(taskList && taskList.name);
-
-//   const title = intl.formatMessage(pageTitles.index);
-
-//   return <Layout title={title}>{/* <Tasks /> */}</Layout>;
-// };
-
-// export default withRouter(Index);
+export default withRouter(Index);
