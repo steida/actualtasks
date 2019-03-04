a3df97fbe469730b3a54373ca9b9dec35aff3f67
Daniel Steogerwald <daniel@steigerwald.cz>
2019-03-04 02:50:32 +0100

    Update index.tsx

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,10 +1,32 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, memo } from 'react';
 import { Text } from 'react-native';
 import Layout from '../components/Layout';
 import useAppContext from '../hooks/useAppContext';
 import useAppStateTaskListByRouter from '../hooks/useAppStateTaskListByRouter';
 import useTaskListTitle from '../hooks/useTaskListTitle';
 import TaskList from '../components/TaskList';
+
+interface LayoutWithTaskListProps {
+  id: string;
+  title: string;
+}
+
+// Micro-optimization exercise. Not ideal, TaskLists in Menu is still
+// rendered on any taskList change. But that's fine.
+const LayoutWithTaskList: FunctionComponent<LayoutWithTaskListProps> = memo(
+  ({ id, title }) => {
+    return (
+      <Layout title={title}>
+        {/* 
+        Reset TaskList via key prop. Not only because of the state, but also
+        because Slate and contentEditable are very complex.
+        https://twitter.com/estejs/status/1102238792382062593
+      */}
+        <TaskList id={id} key={id} />
+      </Layout>
+    );
+  },
+);
 
 const Index: FunctionComponent = () => {
   const { theme } = useAppContext();
@@ -17,16 +39,7 @@ const Index: FunctionComponent = () => {
       </Layout>
     );
 
-  return (
-    <Layout title={title}>
-      {/* 
-        Reset TaskList via key prop. Not only because of the state, but also
-        because Slate and contentEditable are very complex.
-        https://twitter.com/estejs/status/1102238792382062593
-      */}
-      <TaskList taskList={taskList} key={taskList.id} />
-    </Layout>
-  );
+  return <LayoutWithTaskList id={taskList.id} title={title} />;
 };
 
 export default Index;
