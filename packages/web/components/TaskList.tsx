1703fca5b7d6f10d535d4fcd515f136ffe208da6
Daniel Steogerwald <daniel@steigerwald.cz>
2019-03-05 00:01:47 +0100

    Toggle by checkbox click

--- packages/web/components/TaskList.tsx
+++ packages/web/components/TaskList.tsx
@@ -1,5 +1,11 @@
 import { TaskList as TaskListType } from '@app/state/types';
-import React, { FunctionComponent, useState, useMemo } from 'react';
+import React, {
+  FunctionComponent,
+  useMemo,
+  useReducer,
+  Dispatch,
+  useCallback,
+} from 'react';
 import { Editor, RenderNodeProps } from 'slate-react';
 import { Editor as CoreEditor, KeyUtils, Value } from 'slate';
 // @ts-ignore
@@ -11,8 +17,17 @@ import useAppContext from '../hooks/useAppContext';
 import useAppState from '../hooks/useAppState';
 
 type TaskType = TaskListType['slate']['document']['nodes'][0];
-type TaskTypeData = TaskType['data'];
 type TaskTypeTypeProp = TaskType['type'];
+type TaskTypeData = TaskType['data'];
+// Slate key is an implementation detail. It does not belong to TaskData
+// unless we will generate guids somehow.
+type TaskTypeDataWithKey = { key: string } & TaskTypeData;
+type TaskNode = RenderNodeProps['node'];
+
+type Action =
+  | { type: 'update'; payload: Value }
+  | { type: 'toggle'; payload: TaskNode }
+  | { type: 'archive' };
 
 const taskTypeTypeProp: TaskTypeTypeProp = 'task';
 
@@ -25,9 +40,9 @@ type CheckboxProps = Overwrite<
 const Checkbox = (props: CheckboxProps) =>
   createElement('input', { ...props, type: 'checkbox' });
 
-const getTaskData = (node: RenderNodeProps['node']): TaskTypeData => {
-  // data.toJS is costly and we don't need it anyway.
-  // This is simple type safe approach.
+// data.toJS is costly and we don't need it anyway.
+// This is simple type safe approach.
+const getTaskData = (node: TaskNode): TaskTypeData => {
   const completed = 'completed';
   const depth = 'depth';
   return {
@@ -37,42 +52,36 @@ const getTaskData = (node: RenderNodeProps['node']): TaskTypeData => {
 };
 
 interface TaskProps extends RenderNodeProps {
-  foo?: any; // TODO: Dispatcher.
+  dispatch: Dispatch<Action>;
 }
 
-const Task: FunctionComponent<TaskProps> = props => {
+const TaskItem: FunctionComponent<TaskProps> = props => {
   const { theme } = useAppContext();
   const data = getTaskData(props.node);
   // https://reactjs.org/docs/hooks-faq.html#are-hooks-slow-because-of-creating-functions-in-render
   const getTaskDepthStyle = () => {
+    // prettier-ignore
     switch (data.depth) {
-      case 0:
-        return theme.taskDepth0;
-      case 1:
-        return theme.taskDepth1;
-      case 2:
-        return theme.taskDepth2;
-      case 3:
-        return theme.taskDepth3;
-      case 4:
-        return theme.taskDepth4;
-      case 5:
-        return theme.taskDepth5;
-      case 6:
-        return theme.taskDepth6;
-      case 7:
-        return theme.taskDepth7;
-      case 8:
-        return theme.taskDepth8;
-      case 9:
-        return theme.taskDepth9;
-      default:
-        return data.depth < 0 ? theme.taskDepth0 : theme.taskDepth9;
+      case 0: return theme.taskDepth0;
+      case 1: return theme.taskDepth1;
+      case 2: return theme.taskDepth2;
+      case 3: return theme.taskDepth3;
+      case 4: return theme.taskDepth4;
+      case 5: return theme.taskDepth5;
+      case 6: return theme.taskDepth6;
+      case 7: return theme.taskDepth7;
+      case 8: return theme.taskDepth8;
+      case 9: return theme.taskDepth9;
+      default: return data.depth < 0 ? theme.taskDepth0 : theme.taskDepth9;
     }
   };
   const depthStyle = getTaskDepthStyle();
-  // console.log(React.Children.toArray(props.children)[0].props.block.count());
 
+  const handleCheckboxChange = useCallback(() => {
+    props.dispatch({ type: 'toggle', payload: props.node });
+  }, [props]);
+
+  // console.log(React.Children.toArray(props.children)[0].props.block.count());
   return (
     <View {...props.attributes} style={[theme.task, depthStyle]}>
       <View style={theme.taskCheckboxWrapper}>
@@ -83,8 +92,7 @@ const Task: FunctionComponent<TaskProps> = props => {
               data.completed && theme.taskCheckboxCompleted,
             ]}
             checked={data.completed}
-            onChange={() => {}}
-            // onChange={() => props.onCheckboxChange(props.node)}
+            onChange={handleCheckboxChange}
           />
         </div>
       </View>
@@ -101,11 +109,113 @@ interface TaskListProps {
 
 const TaskList: FunctionComponent<TaskListProps> = ({ id }) => {
   const taskListSlate = useAppState(state => state.taskLists[id].slate);
-  const [editorValue, setEditorValue] = useState(() => {
-    // For SSR.
-    KeyUtils.resetGenerator();
-    return Value.fromJSON(taskListSlate);
+
+  const nodeToTaskDataWithKey = (node: TaskNode): TaskTypeDataWithKey => ({
+    key: node.key,
+    ...getTaskData(node),
   });
+
+  const editorRef = React.useRef<Editor>(null);
+
+  const getEditor = () => {
+    const { current } = editorRef;
+    if (current == null) throw new Error('editorRef current is null');
+    return current;
+  };
+
+  const getTaskIndex = (key: string): number => {
+    const editor = getEditor();
+    return editor.value.document.nodes.findIndex(
+      node => node != null && node.key === key,
+    );
+  };
+
+  const tasksWithChildren = (tasks: TaskTypeDataWithKey[]) => {
+    // Dedupe via Map.
+    const map: Map<string, TaskTypeDataWithKey> = new Map();
+    tasks.forEach(task => map.set(task.key, task));
+
+    const getTaskChildren = (
+      task: TaskTypeDataWithKey,
+    ): TaskTypeDataWithKey[] => {
+      const editor = getEditor();
+      const children: TaskTypeDataWithKey[] = [];
+      const { nodes } = editor.value.document;
+      let index = getTaskIndex(task.key);
+      // eslint-disable-next-line no-constant-condition
+      while (true) {
+        index++;
+        const next = nodes.get(index);
+        if (next == null) break;
+        const data = getTaskData(next);
+        if (data.depth <= task.depth) break;
+        children.push({ ...data, key: next.key });
+      }
+      return children;
+    };
+
+    tasks.forEach(task => {
+      getTaskChildren(task).forEach(child => {
+        if (map.has(child.key)) return;
+        map.set(child.key, child);
+      });
+    });
+
+    return [...map.values()];
+  };
+
+  const setNodesData = (tasks: TaskTypeDataWithKey[]) => {
+    const editor = getEditor();
+    tasks.forEach(task => {
+      const { key, ...data } = task;
+      editor.setNodeByKey(
+        key,
+        // @ts-ignore Probably wrong type definition.
+        { data },
+      );
+    });
+  };
+
+  const toggleTasks = (tasks: TaskTypeDataWithKey[]) => {
+    tasks = tasksWithChildren(tasks);
+    const allCompleted = !tasks.some(task => !task.completed);
+    const completedTasks = tasks.map(task => ({
+      ...task,
+      completed: !allCompleted,
+    }));
+    setNodesData(completedTasks);
+  };
+
+  // Reducer here is ok.
+  // https://twitter.com/dan_abramov/status/1102010979611746304
+  const editorValueReducer = (state: Value, action: Action) => {
+    const assertNever = (action: never): never => {
+      throw new Error(`Unexpected action: ${JSON.stringify(action)}`);
+    };
+    switch (action.type) {
+      case 'update':
+        return action.payload;
+      case 'toggle': {
+        toggleTasks([nodeToTaskDataWithKey(action.payload)]);
+        return state;
+      }
+      case 'archive':
+        return state;
+      default:
+        return assertNever(action);
+    }
+  };
+
+  const initReducer = (taskListSlate: TaskListType['slate']) => {
+    KeyUtils.resetGenerator(); // For SSR.
+    return Value.fromJSON(taskListSlate);
+  };
+
+  const [editorValue, dispatch] = useReducer(
+    editorValueReducer,
+    taskListSlate,
+    initReducer,
+  );
 
   const renderNode = (
     props: RenderNodeProps,
@@ -114,12 +224,7 @@ const TaskList: FunctionComponent<TaskListProps> = ({ id }) => {
   ) => {
     switch (props.node.type) {
       case taskTypeTypeProp:
-        return (
-          <Task
-            {...props}
-            // onCheckboxChange={handleTaskCheckboxChange}
-          />
-        );
+        return <TaskItem {...props} dispatch={dispatch} />;
       default:
         return next();
     }
@@ -138,23 +243,22 @@ const TaskList: FunctionComponent<TaskListProps> = ({ id }) => {
 
   const handleEditorChange = ({ value }: { value: Value }) => {
     // TODO: Validate value. Slate can fail. In such case, log it and revert.
-    setEditorValue(value);
+    dispatch({ type: 'update', payload: value });
     const documentHasBeenChanged = value.document !== editorValue.document;
-    if (documentHasBeenChanged) {
-      saveThrottled(value);
-    }
+    if (!documentHasBeenChanged) return;
+    saveThrottled(value);
   };
 
   return (
     <Editor
       // autoFocus does not work reliable.
       autoCorrect={false}
+      spellCheck={false}
       onChange={handleEditorChange}
       // onKeyDown={handleKeyDown}
+      ref={editorRef}
       // ref={editorRef}
       renderNode={renderNode}
-      // spellCheck={false}
-      // @ts-ignore
       value={editorValue}
     />
   );
