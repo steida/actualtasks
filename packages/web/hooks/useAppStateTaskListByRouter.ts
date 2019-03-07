fa41a056c2226986d332c8939d0afdcdd5ec0a35
Daniel Steogerwald <daniel@steigerwald.cz>
2019-03-07 02:32:06 +0100

    Refactor useAppStateTaskListByRouter

--- packages/web/hooks/useAppStateTaskListByRouter.ts
+++ packages/web/hooks/useAppStateTaskListByRouter.ts
@@ -4,14 +4,16 @@ import useAppState from './useAppState';
 
 const useAppStateTaskListByRouter = () => {
   const { router } = useAppContext();
-  const taskLists = useAppState(state => state.taskLists);
   const taskListId =
     (router &&
       router.query &&
       typeof router.query.id === 'string' &&
       router.query.id) ||
     rootTaskListId;
-  return taskLists.find(t => t.id === taskListId);
+  const taskList = useAppState(state =>
+    state.taskLists.find(t => t.id === taskListId),
+  );
+  return taskList;
 };
 
 export default useAppStateTaskListByRouter;
