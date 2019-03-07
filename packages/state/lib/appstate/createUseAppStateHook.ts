2cc570ea4447e4c377c1507fd556e2aab4e29a08
Daniel Steogerwald <daniel@steigerwald.cz>
2019-03-07 23:00:15 +0100

    Update createUseAppStateHook.ts
    Fix race condition and improve checking.

--- packages/state/lib/appstate/createUseAppStateHook.ts
+++ packages/state/lib/appstate/createUseAppStateHook.ts
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState, useCallback } from 'react';
+import { useContext, useEffect, useState, useCallback, useRef } from 'react';
 import AppStateContext, { SetAppState } from './AppStateContext';
 
 // Example:
@@ -7,9 +7,14 @@ import AppStateContext, { SetAppState } from './AppStateContext';
 // const viewer = useAppState(state => state.viewer);
 // const setAppState = useAppState();
 
-// Tips:
-// To optimize useEffect, use useCallback on selector in caller.
-// To bail out of a state update, return null.
+// Tip:
+// To optimize useEffect, use useCallback for the selector.
+
+// As for the implementation, check this:
+// https://reactjs.org/blog/2018/06/07/you-probably-dont-need-derived-state.html
+// https://github.com/facebook/react/tree/master/packages/create-subscription#limitations-in-async-mode
+// https://gist.github.com/bvaughn/d569177d70b50b58bff69c3c4a5353f3
+// Note we don't have to check datasource because local storage is singleton.
 
 interface UseAppState<State> {
   <SelectedState>(selector: (state: State) => SelectedState): SelectedState;
@@ -24,18 +29,32 @@ const createUseAppStateHook = <State extends object>(): UseAppState<State> => {
       return selector ? selector(context.getAppState()) : null;
     }, [context, selector]);
 
-    const [state, setState] = useState(() => getSelectedState());
+    const prevSelectedStateRef = useRef<any>(null);
+    prevSelectedStateRef.current = getSelectedState();
+    // We use prevSelectedStateRef instead of a state.
+    // We use setState only for subscribe change detection.
+    // https://reactjs.org/docs/hooks-reference.html#bailing-out-of-a-state-update
+    const [, setState] = useState(prevSelectedStateRef.current);
+
+    const setSelectedState = useCallback(() => {
+      const selectedState = getSelectedState();
+      // https://reactjs.org/docs/hooks-reference.html#bailing-out-of-a-state-update
+      if (Object.is(selectedState, prevSelectedStateRef.current)) return;
+      prevSelectedStateRef.current = selectedState;
+      setState(selectedState);
+    }, [getSelectedState]);
 
     useEffect(() => {
       if (selector == null) return;
-      return context.subscribe(() => {
-        // We don't have to memo anything.
-        // https://reactjs.org/docs/hooks-reference.html#bailing-out-of-a-state-update
-        setState(getSelectedState());
-      });
-    }, [context, getSelectedState, selector]);
+      const unsubscribe = context.subscribe(setSelectedState);
+      // External values could change between render and mount.
+      // In some cases it may be important to handle this case.
+      // https://gist.github.com/bvaughn/d569177d70b50b58bff69c3c4a5353f3#file-updating-subscriptions-when-props-change-example-js-L62
+      setSelectedState();
+      return unsubscribe;
+    }, [context, selector, setSelectedState]);
 
-    return selector ? state : context.setAppState;
+    return selector ? prevSelectedStateRef.current : context.setAppState;
   };
 
   return useAppState;
