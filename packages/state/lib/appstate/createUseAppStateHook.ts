12dc7027a50ab0352724899085c471ef5a1c9b60
Daniel Steogerwald <daniel@steigerwald.cz>
2019-03-09 01:15:15 +0100

    use useSubscription
    Because subscriptions are tricky.

--- packages/state/lib/appstate/createUseAppStateHook.ts
+++ packages/state/lib/appstate/createUseAppStateHook.ts
@@ -1,5 +1,6 @@
-import { useContext, useEffect, useState, useCallback, useRef } from 'react';
+import { useContext, useMemo } from 'react';
 import AppStateContext, { SetAppState } from './AppStateContext';
+import useSubscription from './useSubscription';
 
 // Example:
 // interface AppState { viewer: Viewer }
@@ -10,11 +11,10 @@ import AppStateContext, { SetAppState } from './AppStateContext';
 // Tip:
 // To optimize useEffect, use useCallback for the selector.
 
-// As for the implementation, check this:
+// As for the implementation:
 // https://reactjs.org/blog/2018/06/07/you-probably-dont-need-derived-state.html
-// https://github.com/facebook/react/tree/master/packages/create-subscription#limitations-in-async-mode
-// https://gist.github.com/bvaughn/d569177d70b50b58bff69c3c4a5353f3
-// Note we don't have to check datasource because local storage is singleton.
+// https://github.com/facebook/react/tree/master/packages/create-subscription
+// https://gist.github.com/bvaughn/e25397f70e8c65b0ae0d7c90b731b189
 
 interface UseAppState<State> {
   <SelectedState>(selector: (state: State) => SelectedState): SelectedState;
@@ -24,39 +24,22 @@ interface UseAppState<State> {
 const createUseAppStateHook = <State extends object>(): UseAppState<State> => {
   const useAppState = (selector?: (state: State) => any) => {
     const context = useContext(AppStateContext);
-
-    const getSelectedState = useCallback(() => {
-      return selector ? selector(context.getAppState()) : null;
-    }, [context, selector]);
-
-    const prevSelectedStateRef = useRef<any>(null);
-    prevSelectedStateRef.current = getSelectedState();
-    // We use prevSelectedStateRef instead of a state.
-    // We use setState only for subscribe change detection.
-    // https://reactjs.org/docs/hooks-reference.html#bailing-out-of-a-state-update
-    const [, setState] = useState(prevSelectedStateRef.current);
-
-    const setSelectedState = useCallback(() => {
-      const selectedState = getSelectedState();
-      // https://reactjs.org/docs/hooks-reference.html#bailing-out-of-a-state-update
-      if (Object.is(selectedState, prevSelectedStateRef.current)) return;
-      prevSelectedStateRef.current = selectedState;
-      setState(selectedState);
-    }, [getSelectedState]);
-
-    useEffect(() => {
-      if (selector == null) return;
-      const unsubscribe = context.subscribe(setSelectedState);
-      // External values could change between render and mount.
-      // In some cases it may be important to handle this case.
-      // https://gist.github.com/bvaughn/d569177d70b50b58bff69c3c4a5353f3#file-updating-subscriptions-when-props-change-example-js-L62
-      setSelectedState();
-      return unsubscribe;
-    }, [context, selector, setSelectedState]);
-
-    return selector ? prevSelectedStateRef.current : context.setAppState;
+    const value = useSubscription(
+      useMemo(
+        () => ({
+          source: context,
+          getCurrentValue: () => {
+            return selector ? selector(context.getAppState()) : null;
+          },
+          subscribe: (_source: any, callback: any) => {
+            return context.subscribe(callback);
+          },
+        }),
+        [context, selector],
+      ),
+    );
+    return selector ? value : context.setAppState;
   };
-
   return useAppState;
 };
 
---
+++ packages/state/lib/appstate/useSubscription.ts
@@ -0,0 +1,101 @@
+import { useEffect, useState } from 'react';
+
+// https://gist.githubusercontent.com/bvaughn/e25397f70e8c65b0ae0d7c90b731b189/raw/ccc3cc72b72a619917dd2f7728bdebac2e3131c1/useSubscription.js
+
+// Hook used for safely managing subscriptions in concurrent mode.
+//
+// In order to avoid removing and re-adding subscriptions each time this hook is called,
+// the parameters passed to this hook should be memoized in some way–
+// either by wrapping the entire params object with useMemo()
+// or by wrapping the individual callbacks with useCallback().
+const useSubscription = ({
+  // This is the thing being subscribed to (e.g. an observable, event dispatcher, etc).
+  source,
+
+  // (Synchronously) returns the current value of our subscription source.
+  getCurrentValue,
+
+  // This function is passed an event handler to attach to the subscription source.
+  // It should return an unsubscribe function that removes the handler.
+  subscribe,
+}: {
+  source: any;
+  getCurrentValue: (source: any) => any;
+  subscribe: any;
+}) => {
+  // Read the current value from our subscription source.
+  // When this value changes, we'll schedule an update with React.
+  // It's important to also store the source itself so that we can check for staleness.
+  // (See the comment in checkForUpdates() below for more info.)
+  const [state, setState] = useState({
+    source,
+    value: getCurrentValue(source),
+  });
+
+  // If the source has changed since our last render, schedule an update with its current value.
+  if (state.source !== source) {
+    setState({
+      source,
+      value: getCurrentValue(source),
+    });
+  }
+
+  // It is important not to subscribe while rendering because this can lead to memory leaks.
+  // (Learn more at reactjs.org/docs/strict-mode.html#detecting-unexpected-side-effects)
+  // Instead, we wait until the commit phase to attach our handler.
+  //
+  // We intentionally use a passive effect (useEffect) rather than a synchronous one (useLayoutEffect)
+  // so that we don't stretch the commit phase.
+  // This also has an added benefit when multiple components are subscribed to the same source:
+  // It allows each of the event handlers to safely schedule work without potentially removing an another handler.
+  // (Learn more at https://codesandbox.io/s/k0yvr5970o)
+  useEffect(() => {
+    let didUnsubscribe = false;
+
+    const checkForUpdates = () => {
+      // It's possible that this callback will be invoked even after being unsubscribed,
+      // if it's removed as a result of an event/update from the source.
+      // In this case, React will log a DEV warning about an update from an unmounted component.
+      // We can avoid triggering that warning with this check.
+      if (didUnsubscribe) {
+        return;
+      }
+
+      setState(prevState => {
+        // Ignore values from stale sources!
+        // Since we subscribe an unsubscribe in a passive effect,
+        // it's possible that this callback will be invoked for a stale (previous) source.
+        // This check avoids scheduling an update for htat stale source.
+        if (prevState.source !== source) {
+          return prevState;
+        }
+
+        // Some subscription sources will auto-invoke the handler, even if the value hasn't changed.
+        // If the value hasn't changed, no update is needed.
+        // Return state as-is so React can bail out and avoid an unnecessary render.
+        const value = getCurrentValue(source);
+        if (prevState.value === value) {
+          return prevState;
+        }
+
+        return { ...prevState, value };
+      });
+    };
+    const unsubscribe = subscribe(source, checkForUpdates);
+
+    // Because we're subscribing in a passive effect,
+    // it's possible that an update has occurred between render and our effect handler.
+    // Check for this and schedule an update if work has occurred.
+    checkForUpdates();
+
+    return () => {
+      didUnsubscribe = true;
+      unsubscribe();
+    };
+  }, [getCurrentValue, source, subscribe]);
+
+  // Return the current value for our caller to use while rendering.
+  return state.value;
+};
+
+export default useSubscription;
