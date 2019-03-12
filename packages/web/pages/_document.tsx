d2196ae138b71362737ae510cdbaad6f088cdd88
Daniel Steogerwald <daniel@steigerwald.cz>
2019-03-13 00:12:21 +0100

    Add favicon
    Thank you https://favicon.io/emoji-favicons/heavy-check-mark/

--- packages/web/pages/_document.tsx
+++ packages/web/pages/_document.tsx
@@ -37,6 +37,24 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/static/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/static/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/static/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/static/site.webmanifest" />
         </Head>
         <body style={{ height: '100%', overflow: 'hidden' }}>
           <Main />
