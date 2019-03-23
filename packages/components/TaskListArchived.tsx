c2ca6c2befbb8ff307816be68a0e5ea1453b47c5
Daniel Steogerwald <daniel@steigerwald.cz>
2019-03-23 01:57:20 +0100

    Update TaskListArchived.tsx

--- packages/components/TaskListArchived.tsx
+++ packages/components/TaskListArchived.tsx
@@ -1,14 +1,50 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useCallback, useMemo } from 'react';
 import { Text } from 'react-native';
 import useAppContext from '@app/hooks/useAppContext';
+import useAppState from '@app/hooks/useAppState';
+import { AppState } from '@app/state/types';
+import TaskListBar from './TaskListBar';
+import { LayoutScrollView } from './Layout';
 
 interface TaskListArchivedProps {
   taskListId: string;
 }
 
-const TaskListArchived: FunctionComponent<TaskListArchivedProps> = () => {
+const TaskListArchived: FunctionComponent<TaskListArchivedProps> = ({
+  taskListId,
+}) => {
   const { theme } = useAppContext();
-  return <Text style={theme.text}>TaskListArchived</Text>;
+  const archivedSlate = useAppState(
+    useCallback(
+      ({ taskLists }: AppState) => {
+        const taskList = taskLists.find(t => t.id === taskListId);
+        return taskList != null ? taskList.archivedSlate : null;
+      },
+      [taskListId],
+    ),
+  );
+
+  const sortedTasks = useMemo(() => {
+    return archivedSlate == null
+      ? []
+      : // Sort mutates, therefore we need shallow clone.
+        [...archivedSlate.document.nodes].sort((a, b) => {
+          if (a.data.completedAt == null || b.data.completedAt == null)
+            return 0;
+          return a.data.completedAt - b.data.completedAt;
+        });
+  }, [archivedSlate]);
+
+  return (
+    <>
+      <TaskListBar hasCompletedTask={false} dispatch={() => {}} />
+      <LayoutScrollView>
+        {sortedTasks.map(task => {
+          return <Text style={theme.text}>{task.type}</Text>;
+        })}
+      </LayoutScrollView>
+    </>
+  );
 };
 
 export default TaskListArchived;
